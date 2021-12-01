ceadc3cceea7ff965af95e35d749b5821ad80e1d
Francesca Pompi <francesca.pompi93@gmail.com>
2021-12-01 16:09:25 +0100

    aggiunti desideri alla lista

--- src/org/generation/italy/christmas/Main.java
+++ src/org/generation/italy/christmas/Main.java
@@ -1,10 +1,28 @@
 package org.generation.italy.christmas;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// creo l'array
+		ArrayList<String> listaDesideri = new ArrayList<String>();
+		
+		Scanner scan = new Scanner(System.in);
 
+		String scelta;
+		do {
+			System.out.print("Aggiungi elemento alla lista dei desideri: ");
+			String desiderio = scan.next();
+			listaDesideri.add(desiderio);
+			System.out.println("La tua lista contiene " + listaDesideri.size() + " desideri");
+			System.out.print("Continuare? (s/n) ");
+			scelta = scan.next();
+		} while(scelta != "s");
+		
+		
 	}
 
 }
